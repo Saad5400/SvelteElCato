19a47c7e680065d596d473d04e1e30030cc64776
Saad5400 <sdbtwa@gmail.com>
2024-02-27 18:54:03 +0300

    changed font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,10 @@
 
 <div class="container max-h-full">
     <Hero/>
+
+    <div>
+        <h3 class="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+            الدورات
+        </h3>
+    </div>
 </div>
