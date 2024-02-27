10d50ccb3a9c50443bc651e2c2915535d1d41d47
Saad5400 <sdbtwa@gmail.com>
2024-02-27 22:57:36 +0300

    much better hacking effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,25 +4,17 @@
     import Navbar from "$lib/components/navbar/Navbar.svelte";
 
     import Hacking from "$lib/components/index/Hacking.svelte";
-    import StaticTV from "$lib/components/index/StaticTV.svelte";
 
     let isHacking = true;
-    let isStaticTV = false;
 
     setTimeout(() => {
         isHacking = false;
-        isStaticTV = true;
-        setTimeout(() => {
-            isStaticTV = false;
-        }, 500);
-    }, 2500);
+    }, 1500);
 </script>
 
 <ModeWatcher/>
 {#if isHacking}
     <Hacking/>
-{:else if isStaticTV}
-    <StaticTV/>
 {:else}
     <Navbar title="الرئيسية" url="/"/>
     <slot/>
