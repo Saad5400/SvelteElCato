195167ba56289135108f583ce15233a23ce83a03
Saad5400 <sdbtwa@gmail.com>
2024-02-29 18:57:36 +0300

    updated layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,9 @@
     import "../app.pcss";
     import {ModeWatcher} from "mode-watcher";
     import Navbar from "$lib/components/navbar/Navbar.svelte";
-    import Hacking from "$lib/components/index/Hacking.svelte";
-
-    let isHacking = false; // TODO: Make it true
-
-    setTimeout(() => {
-        isHacking = false;
-    }, 1500);
 </script>
 
 <ModeWatcher/>
-{#if isHacking}
-    <Hacking/>
-{:else}
-    <Navbar title="الرئيسية" url="/"/>
-    <slot/>
-{/if}
+
+<Navbar title="الرئيسية" url="/"/>
+<slot/>
