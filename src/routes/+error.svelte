d182df5b57a2e0f156eee0f658ccf03d1047d17c
Saad5400 <sdbtwa@gmail.com>
2024-03-01 16:49:47 +0300

    much better errors

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
     import {page} from '$app/stores';
+
+    function getArabicMessage(code: number) {
+        switch (code) {
+            case 400:
+                return 'الطلب غير متوافق مع البروتوكول، يلزم التصحيح وإعادة الإرسال';
+            case 401:
+                return "تحذير! تم تفعيل الدفاعات الإلكترونية - الدخول ممنوع";
+            case 403:
+                return "تحذير! تم تفعيل الدفاعات الإلكترونية - الدخول ممنوع";
+            case 404:
+                return "الرابط المتبع أدى إلى فراغ رقمي، المعلومات المطلوبة غير مستقرة في هذا العنوان";
+            case 500:
+                return "تعطل في النواة الرئيسية للخادم، جارٍ تحليل السبب الجذري";
+            default:
+                return 'استثناء غير معالج في نواة النظام';
+        }
+    }
+
+
+    $: code = $page.status;
+    // $: message = $page.error!.message;
+    $: message = getArabicMessage(code);
 </script>
 
-<div dir="ltr" class="flex flex-col justify-center items-center min-w-[100dvw] min-h-screen absolute top-0 bottom-0 left-0 right-0 z-50 bg-destructive text-destructive-foreground gap-2">
-    <h1>{$page.status}</h1>
-    <h2>{$page.error.message}</h2>
+<div class="text-center flex flex-col justify-center items-center min-w-[100dvw] min-h-screen absolute top-0 bottom-0 left-0 right-0 z-50 bg-destructive text-destructive-foreground gap-2">
+    <h1>{code}</h1>
+    <h2>{message}</h2>
 </div>
