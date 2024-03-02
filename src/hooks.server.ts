634910f72f97ac20b20c76db67284c97c61c8fef
Saad5400 <sdbtwa@gmail.com>
2024-03-02 18:15:30 +0300

    still trying

---
+++ src/hooks.server.ts
@@ -0,0 +1,16 @@
+import type {Handle} from '@sveltejs/kit';
+
+export const handle: Handle = async ({event, resolve}) => {
+    const response = await resolve(event, {
+        preload: ({type, path}) => {
+            // Preload specific fonts along with the default .js and .css files
+            if (type === 'font' && (path.endsWith('Handjet.ttf'))) {
+                return true; // Return true to preload this font
+            }
+            // Keep the default preloading behavior for .js and .css files
+            return type === 'js' || type === 'css';
+        }
+    });
+
+    return response;
+};
