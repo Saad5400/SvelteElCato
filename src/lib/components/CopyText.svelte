4534a2b329fd01c16869f669b1a3994c84b6125b
Saad5400 <sdbtwa@gmail.com>
2024-03-02 07:24:36 +0300

    copy toast

--- src/lib/components/CopyText.svelte
+++ src/lib/components/CopyText.svelte
@@ -1,11 +1,15 @@
-<script>
+<script lang="ts">
     import {Button} from "$lib/components/ui/button";
     import {Copy} from "lucide-svelte";
+    import {toast} from "svelte-sonner";
 
-    export let copyText;
+    export let copyText: string;
 </script>
 
-<Button variant="ghost" class="gap-2" on:click={() => navigator.clipboard.writeText(copyText)}>
+<Button variant="ghost" class="gap-2" on:click={() => {
+    navigator.clipboard.writeText(copyText);
+    toast.success(`تم نسخ '${copyText}' إلى الحافظة`);
+}}>
     <Copy class="h-3 w-3"/>
     <slot/>
 </Button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
-<script>
+<script lang="ts">
     import "../app.pcss";
     import {ModeWatcher} from "mode-watcher";
     import Navbar from "$lib/components/Navbar.svelte";
     import {ProgressBar} from "@prgm/sveltekit-progress-bar";
+    import {Toaster} from "$lib/components/ui/sonner";
 </script>
 
 <ModeWatcher/>
 
+<Toaster class="max-w-[20rem]"/>
 <ProgressBar class="text-accent-foreground" zIndex={100}/>
 <Navbar>
     <slot/>
