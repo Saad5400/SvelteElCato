d40ca3e4c957461abb38c31c932266a357aeb692
Saad5400 <sdbtwa@gmail.com>
2024-03-02 05:48:11 +0300

    added lading progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
     import "../app.pcss";
     import {ModeWatcher} from "mode-watcher";
     import Navbar from "$lib/components/Navbar.svelte";
+    import {ProgressBar} from "@prgm/sveltekit-progress-bar";
 </script>
 
 <ModeWatcher/>
 
 <Navbar/>
+<ProgressBar class="text-accent-foreground" zIndex={100}/>
 <slot/>
