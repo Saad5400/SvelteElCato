6d9a9793c6b6be355df4349569397f66565c0eab
Saad5400 <sdbtwa@gmail.com>
2024-03-04 01:55:55 +0300

    sorting the steps of a track

--- src/lib/models/Track.ts
+++ src/lib/models/Track.ts
@@ -14,6 +14,6 @@ export default class Track extends BaseModel {
         this.urlName = data.urlName;
         this.description = data.description;
         this.order = data.order;
-        this.steps = data.expand?.steps?.map((step: any) => new Step(step)) || [];
+        this.steps = data.expand?.steps?.map((step: any) => new Step(step)).sort((a: Step, b: Step) => a.order - b.order) || [];
     }
 }
