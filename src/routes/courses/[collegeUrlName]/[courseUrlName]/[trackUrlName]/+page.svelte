0035a55c1ef95f54306ff8516415780e6e1b13e2
Saad5400 <sdbtwa@gmail.com>
2024-03-05 09:32:30 +0300

    better track menu hint

--- src/routes/courses/[collegeUrlName]/[courseUrlName]/[trackUrlName]/+page.svelte
+++ src/routes/courses/[collegeUrlName]/[courseUrlName]/[trackUrlName]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type {PageData} from './$types';
     import {Button} from "$lib/components/ui/button";
+    import { Menu } from 'lucide-svelte';
 
     export let data: PageData;
 </script>
@@ -13,7 +14,13 @@
 
 <div class="flex items-center justify-center mx-auto min-h-screen-without-navbar">
     {#if data.track.steps && data.track.steps.length > 0}
-        اختر احد الدروس من القائمة باليمين
+        <span class="hidden lg:contents">
+            اختر احد الدروس من القائمة باليمين
+        </span>
+        <span class="contents lg:hidden">
+            افتح قائمة الدروس من اعلى يمين الصفحة بالضغط على
+            <Menu class="w-3 h-3 mx-2" />
+        </span>
     {:else}
         لا يوجد دروس حاليا في هذا المسار
     {/if}
