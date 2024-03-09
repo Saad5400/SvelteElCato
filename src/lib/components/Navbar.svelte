dd6af288b9467b4de775cadbdec1eeed6fcec786
Saad5400 <sdbtwa@gmail.com>
2024-03-09 10:24:11 +0300

    idk just trying transition

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -90,10 +90,10 @@
             <nav class="flex items-center">
                 <Button on:click={toggleMode} variant="ghost" size="icon">
                     <Sun
-                        class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                        class="absolute dark:rotate-90 dark:scale-0 rotate-0 scale-100"
                     />
                     <Moon
-                        class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+                        class="absolute rotate-90 scale-0 dark:rotate-0 dark:scale-100"
                     />
                     <span class="sr-only">Toggle theme</span>
                 </Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import {fontFamily} from "tailwindcss/defaultTheme";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
