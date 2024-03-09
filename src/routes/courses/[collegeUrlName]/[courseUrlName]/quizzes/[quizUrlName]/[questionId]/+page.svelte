8e47deb2c9e938ddb35052a40fdbeb698fe240d2
Saad5400 <sdbtwa@gmail.com>
2024-03-09 07:22:22 +0300

    question explanation

--- src/routes/courses/[collegeUrlName]/[courseUrlName]/quizzes/[quizUrlName]/[questionId]/+page.svelte
+++ src/routes/courses/[collegeUrlName]/[courseUrlName]/quizzes/[quizUrlName]/[questionId]/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import type {PageData} from './$types';
     import Article from "$lib/components/Article.svelte";
-    import Card from "$lib/components/cardsView/Card.svelte";
-    import type {ButtonEventHandler} from "bits-ui/dist/bits/button";
+    import type {ButtonEventHandler} from "$lib/components/ui/button";
     import {Button} from "$lib/components/ui/button";
+    import {Info} from "lucide-svelte";
+    import {page} from "$app/stores";
+    import * as Drawer from "$lib/components/ui/drawer";
 
     function choiceClicked(event: ButtonEventHandler<MouseEvent>, name: string, clicked: HTMLElement) {
         if (name == data.question.correct) {
             clicked.classList.add("bg-green-500");
             clicked.classList.add("hover:bg-green-600");
+            showExplanation = true;
         } else {
             clicked.classList.add("bg-destructive");
             clicked.classList.add("hover:bg-destructive/70");
@@ -16,6 +19,8 @@
     }
 
     export let data: PageData;
+    let showExplanation = false;
+    $: if($page.url.href) showExplanation = false;
 </script>
 
 <svelte:head>
@@ -28,12 +33,30 @@
      dir="ltr">
     <div class="w-full flex-1 flex flex-col items-center justify-start text-center gap-8">
         <Article content={data.question.content}/>
-        <div class="w-full grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-4 p-2 md:p-4 lg:p-8 xl:p-16">
+        <div class="w-full grid grid-cols-1 md:grid-cols-2 gap-4 p-2 md:p-4 lg:p-8 xl:p-16">
             {#each data.question.options() as option, index}
-                <Button variant="outline3D" class="h-full flex flex-col min-w-64 text-lg"
-                        on:click={(e) => choiceClicked(e, option.name, e.currentTarget)}>
-                    <Article content={option.content}/>
-                </Button>
+                <div class="indicator">
+                    {#if data.question.explanation && option.name === data.question.correct && showExplanation}
+                        <Drawer.Root>
+                            <Drawer.Trigger>
+                                <Button variant="secondary" class="indicator-item" size="icon">
+                                    <Info class="w-3 h-3"/>
+                                </Button>
+                            </Drawer.Trigger>
+                            <Drawer.Content>
+                                <Drawer.Header>
+                                    <Drawer.Description class="flex flex-row items-center justify-center min-h-60">
+                                        <Article content={data.question.explanation} />
+                                    </Drawer.Description>
+                                </Drawer.Header>
+                            </Drawer.Content>
+                        </Drawer.Root>
+                    {/if}
+                    <Button variant="outline3D" class="text-lg h-full w-full"
+                            on:click={(e) => choiceClicked(e, option.name, e.currentTarget)}>
+                        <Article content={option.content}/>
+                    </Button>
+                </div>
             {/each}
         </div>
     </div>
@@ -52,3 +75,20 @@
         </div>
     {/if}
 </div>
+
+<style lang="postcss">
+    .indicator {
+        & :where(.indicator-item) {
+            @apply bottom-auto end-0 start-auto top-0 -translate-y-5 translate-x-1/3;
+        }
+    }
+
+    .indicator {
+        @apply relative inline-flex;
+
+        & :where(.indicator-item) {
+            z-index: 1;
+            @apply absolute transform whitespace-nowrap;
+        }
+    }
+</style>
