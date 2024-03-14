4e5c708398730c2cee20a3fe2a45bbb6265bd190
Saad5400 <sdbtwa@gmail.com>
2024-03-14 09:07:01 +0300

    fixed overflow

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
-    import {mode} from "mode-watcher";
+    import { mode } from "mode-watcher";
 
     export let content: string;
 </script>
+
+<svelte:head>
+    {#if $mode === "dark"}
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/bright.min.css"
+        />
+    {:else}
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/classic-light.min.css"
+        />
+    {/if}
+</svelte:head>
+
+<article dir="ltr" class="break-words text-start">
+    {@html content}
+</article>
 
 <style lang="postcss">
     :global(article a) {
@@ -10,31 +28,17 @@
     }
 
     :global(
-        article h1,
-        article h2,
-        article h3,
-        article h4,
-        article h5,
-        article h6
-    ) {
+            article h1,
+            article h2,
+            article h3,
+            article h4,
+            article h5,
+            article h6
+        ) {
         @apply my-4;
     }
 
     :global(article code) {
-        @apply whitespace-pre-wrap !px-0 bg-accent-foreground/15;
+        @apply whitespace-pre-wrap bg-accent-foreground/15  break-all;
     }
 </style>
-
-<svelte:head>
-    {#if $mode === 'dark'}
-        <link rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/bright.min.css"/>
-    {:else}
-        <link rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/classic-light.min.css"/>
-    {/if}
-</svelte:head>
-
-<article dir="ltr" class="break-words text-start">
-    {@html content}
-</article>
