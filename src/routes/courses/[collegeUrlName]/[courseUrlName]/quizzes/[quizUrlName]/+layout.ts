90d80b3c5c964cceb5b9192f80cd4640d2c7a7d4
Saad5400 <sdbtwa@gmail.com>
2024-03-15 20:03:18 +0300

    better questions loading

--- src/routes/courses/[collegeUrlName]/[courseUrlName]/quizzes/[quizUrlName]/+layout.ts
+++ src/routes/courses/[collegeUrlName]/[courseUrlName]/quizzes/[quizUrlName]/+layout.ts
@@ -1,12 +1,25 @@
-import type {LayoutLoad} from './$types';
+import type { LayoutLoad } from './$types';
 import Track from "$lib/models/Track";
-import navStore, {type NavItem} from "$lib/stores/navStore";
+import navStore, { type NavItem } from "$lib/stores/navStore";
 import { error } from '@sveltejs/kit';
+import { handleError } from '$lib/models/TypedPocketBase';
+import Quiz from '$lib/models/Quiz';
 
-export const load: LayoutLoad = async ({parent, params, fetch}) => {
-    const {course} = await parent();
+export const load: LayoutLoad = async ({ parent, params, fetch }) => {
+    const { course, pb } = await parent();
 
-    const quiz = course.quizzes.find(q => q.urlName === params.quizUrlName) || error(404, 'Quiz not found');
+    let quiz = course.quizzes.find(q => q.urlName === params.quizUrlName) || error(404, 'Quiz not found');
+    let quizWithQuestions = await pb.collection('quizzes')
+        .getFirstListItem(
+            pb.filter("id = {:quizId}", { quizId: quiz.id }),
+            {
+                expand: 'questions',
+                fetch: async (url, config) => fetch(url, config)
+            }
+        )
+        .catch(handleError);
+
+    quiz = new Quiz(quizWithQuestions);
 
     return {
         quiz
