891b4ee13ee9bd50057cdfaeefcec58c2cc47f10
Saad5400 <sdbtwa@gmail.com>
2024-03-31 17:15:55 +0300

    init loading

--- src/lib/components/InitLoading.svelte
+++ src/lib/components/InitLoading.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import Loading from "./icons/Loading.svelte";
 
   const count = 50;
   let div: HTMLDivElement;
@@ -7,16 +8,24 @@
   onMount(async () => {
     div.style.scale = "0";
     div.style.pointerEvents = "none";
-    await new Promise((resolve) => setTimeout(resolve, 150));
+    await new Promise((resolve) => setTimeout(resolve, 500));
     document.body.style.overflowY = "auto";
     div.remove();
   });
 </script>
 
 <div
-  class="bg-accent fixed z-[999] flex flex-col min-h-[100dvh] min-w-[100dvw] scale-100 flex-row flex-wrap items-center justify-center text-center !font-mono text-4xl transition-all duration-500"
+  id="init-loading"
+  class="fixed z-[999] flex min-h-[100dvh] min-w-[100dvw] scale-100 flex-col flex-wrap items-center justify-center bg-background text-center duration-500"
   dir="ltr"
   bind:this={div}
 >
-  Initializing ...
+  <Loading class="w-24" />
 </div>
+
+<style lang="postcss">
+  #init-loading {
+    @apply transition-none;
+    transition: scale 0.1s ease-in-out;
+  }
+</style>
