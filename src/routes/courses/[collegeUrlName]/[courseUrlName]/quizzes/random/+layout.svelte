52310d0d17d18b73ad35b72198b5580729fa8ec7
Saad5400 <sdbtwa@gmail.com>
2024-06-02 16:43:18 +0300

    menu

--- src/routes/courses/[collegeUrlName]/[courseUrlName]/quizzes/random/+layout.svelte
+++ src/routes/courses/[collegeUrlName]/[courseUrlName]/quizzes/random/+layout.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
   import type { LayoutData } from "./$types";
+  import navStore, { type NavItem } from "$lib/stores/navStore";
+  import { onDestroy, onMount } from "svelte";
+  import { persisted } from "svelte-persisted-store";
 
   export let data: LayoutData;
+
+  const solvedStore = persisted("solvedQuestions", [] as string[]);
+  const markedStore = persisted("markedQuestions", [] as string[]);
+
+  onMount(() => {
+    const navItems: NavItem[] = data.questions.map((question_id, index) => {
+      return {
+        title: index + 1 + "",
+        href: `/courses/${data.course.college.urlName}/${data.course.urlName}/quizzes/random/${question_id}`,
+        class:
+          ($solvedStore.includes(question_id) ? "correct " : " ") +
+          ($markedStore.includes(question_id) ? "marked" : ""),
+      };
+    });
+    navStore.set({
+      title: "جميع الأسئلة عشوائيا",
+      items: navItems,
+    });
+  });
+
+  onDestroy(() => {
+    navStore.set({ title: "", items: [] });
+  });
 </script>
 
 <div id="quizLayout" class="flex flex-1 flex-row overflow-x-clip">
