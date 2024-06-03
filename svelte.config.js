b922a9e0c25c0953d0ba0a0f59807c8da946712c
Saad5400 <sdbtwa@gmail.com>
2024-06-03 16:00:11 +0300

    switched to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   // @ts-ignore
   onwarn: (warning, handler) => {
@@ -19,7 +19,12 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or, you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      split: true,
+      runtime: "edge",
+      regions: ["cdg1"],
+      maxDuration: 60,
+    }),
   },
 };
 
