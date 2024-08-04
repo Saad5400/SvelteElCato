e4db6f7d33fe1b4cc5a4d69ade5e0a2ad93ce196
Saad5400 <sdbtwa@gmail.com>
2024-08-05 02:33:43 +0300

    reviews component

---
+++ src/routes/reviews/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Reviews from "../courses/[courseUrlName]/subscribe/Reviews.svelte";
+</script>
+
+<main class="container max-w-screen-md flex flex-col gap-8">
+  <h1>
+    تجارب المشتركين
+  </h1>
+  <Reviews />
+</main>
