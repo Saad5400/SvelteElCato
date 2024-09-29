e76b2bdf2d3ee674b1e105c45df1ddf6be5f4f08
Saad5400 <sdbtwa@gmail.com>
2024-09-29 22:12:17 +0300

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,21 +3,12 @@
   import { ModeWatcher } from "mode-watcher";
   import Navbar from "$lib/components/navbar/Navbar.svelte";
   import { Toaster } from "$lib/components/ui/sonner";
-  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-  import { inject as injectAnalytics } from "@vercel/analytics";
   import usePageTransition from "$lib/hooks/usePageTransition";
   import { ProgressBar } from "@prgm/sveltekit-progress-bar";
   import type { LayoutData } from "./$types";
-  import IntroVideo from "$lib/components/IntroVideo.svelte";
-  import { PUBLIC_ENVIRONMENT } from "$env/static/public";
-  import { onMount } from "svelte";
-  import useBrowserFingerprint from "$lib/hooks/useBrowserFingerprint";
-  import type FingerPrint from "$lib/models/FingerPrint";
 
 
   usePageTransition();
-  injectSpeedInsights();
-  injectAnalytics();
 
   export let data: LayoutData;
 </script>
