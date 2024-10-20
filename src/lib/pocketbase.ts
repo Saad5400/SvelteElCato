64f4284cf3c41717750578b7757debeba97cf4a3
Saad5400 <sdbtwa@gmail.com>
2024-10-20 03:02:28 +0300

    trying without AbortController

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -2,15 +2,15 @@ import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
 import PocketBase, { type SendOptions } from "pocketbase";
 import type TypedPocketBase from "$lib/models/TypedPocketBase";
 // @ts-ignore
-import { AbortController, abortableFetch } from "abortcontroller-polyfill/dist/cjs-ponyfill";
+// import { AbortController, abortableFetch } from "abortcontroller-polyfill/dist/cjs-ponyfill";
 
 export function createPbInstance(_fetch: any) {
 
-  const { fetch } = abortableFetch(_fetch);
+  // const { fetch } = abortableFetch(_fetch);
   const pb = new PocketBase(PUBLIC_POCKETBASE_URL) as TypedPocketBase;
 
   pb.beforeSend = function(url: string, options: SendOptions) {
-    options.fetch = fetch;
+    options.fetch = _fetch;
 
     return { url, options };
   };
