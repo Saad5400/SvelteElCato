a878c6d92b0bc856e762f950ea1301328bb1d39e
Saad Batwa <86385454+Saad5400@users.noreply.github.com>
2025-02-24 21:41:47 +0300

    Update Reviews.svelte

--- src/routes/courses/[courseUrlName]/subscribe/Reviews.svelte
+++ src/routes/courses/[courseUrlName]/subscribe/Reviews.svelte
@@ -4,31 +4,27 @@
     وذاكرت واتدربت وطبقت معايا، باذن الله بتجيب اعلى الدرجات
   </p>
 
-  <!-- برمجة 1 دفعة 45 -->
+  <p class="font-bold">التفكير الحاسوبي دفعة 46</p>
+  <hr />
+  {#each Array.from({ length: 8 }, (_, i) => i) as i}
+    <img src={`/images/reviews/46/se1011/${i + 1}.jpeg`} alt="review" />
+  {/each}
+
+  <p class="font-bold">برمجة 1 دفعة 46</p>
+  <hr />
+  {#each Array.from({ length: 15 }, (_, i) => i) as i}
+    <img src={`/images/reviews/46/cs1211/${i + 1}.jpeg`} alt="review" />
+  {/each}
+
   <p class="font-bold">برمجة 1 دفعة 45</p>
   <hr />
   {#each Array.from({ length: 12 }, (_, i) => i) as i}
     <img src={`/images/reviews/45/cs1211/${i + 1}.jpeg`} alt="review" />
   {/each}
 
-  <!-- برمجة 2 دفعة 45 -->
   <p class="font-bold">برمجة 2 دفعة 45</p>
   <hr />
-  {#each Array.from({ length: 27 }, (_, i) => i} as i}
+  {#each Array.from({ length: 27 }, (_, i) => i) as i}
     <img src={`/images/reviews/45/cs1312/${i + 1}.jpg`} alt="review" />
-  {/each}
-
-  <!-- التفكير الحاسوبي دفعة 46 -->
-  <p class="font-bold">التفكير الحاسوبي دفعة 46</p>
-  <hr />
-  {#each Array.from({ length: 8 }, (_, i) => i} as i}
-    <img src={`/images/reviews/46/se1011/${i + 1}.jpg`} alt="review" />
-  {/each}
-
-  <!-- برمجة 1 دفعة 46 -->
-  <p class="font-bold">برمجة 1 دفعة 46</p>
-  <hr />
-  {#each Array.from({ length: 15 }, (_, i) => i} as i}
-    <img src={`/images/reviews/46/cs1211/${i + 1}.jpg`} alt="review" />
   {/each}
 </div>
